a4708772857a826f5f784742844d3b910b08ce8d
Darshan Gowda G <108842799+D2Gowda@users.noreply.github.com>
2022-09-17 19:45:46 +0530

    D

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link rel="stylesheet" href="./CSS/c1.css">
+    <link rel="stylesheet" href="./CSS/style.css">
 </head>
 <body>
     <nav>
