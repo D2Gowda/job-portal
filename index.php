d46b60119b4f52056cdf859561890f378df351e0
Darshan Gowda G <108842799+D2Gowda@users.noreply.github.com>
2022-09-17 19:37:16 +0530

    D

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="./CSS/c1.css">
+</head>
+<body>
+    <nav>
+        <label class="logo">Applex</label>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">register</a></li>
+        </ul>
+    </nav>
+    <div class="content">
+            <img src="./IMAGE/img1.jpg">
+    </div>
+    <style>
+        .content img{
+            height: 90vh;
+            width: 100%;
+         }
+    </style>
+    <?php include('footer.php')?>
+</body>
+</html>
